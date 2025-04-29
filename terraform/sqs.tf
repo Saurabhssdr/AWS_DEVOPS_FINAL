27df152c147eb36f9cffcbea7458401eb4cf4124
Saurabhssdr <124699813+Saurabhssdr@users.noreply.github.com>
2025-04-29 11:30:38 +0530

    Update sqs.tf

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -1,11 +1,10 @@
-# Create the SQS queue
+
 resource "aws_sqs_queue" "csv_cleanup_queue" {
   name                       = "csv-upload-cleanup-queue"
   delay_seconds              = 120  # 2-minute delay
   visibility_timeout_seconds = 300
 }
 
-# Connect the SQS queue to the already defined Lambda function
 resource "aws_lambda_event_source_mapping" "sqs_trigger" {
   event_source_arn = aws_sqs_queue.csv_cleanup_queue.arn
   function_name    = aws_lambda_function.sqs_trigger_lambda.function_name
